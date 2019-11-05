ab6db136be5cb49cf2955659553f32b2e1cc420d
Oleg Repin <iamolegga@gmail.com>
2019-11-06 00:47:04 +0300

    add `true` value for `Params`

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { Reflector } from '@nestjs/core';
 
 const roleReflectionToken = 'nestjs-roles';
 
-type RoleParams<T> = [T, ...T[]] | [false];
+type RoleParams<T> = [T, ...T[]] | [boolean];
 
 export interface RolesGuardStatic<R> {
   Params(...allowedRoles: RoleParams<R>): ReturnType<typeof SetMetadata>;
@@ -22,9 +22,11 @@ export function createRolesGuard<R>(
   return class RolesGuard implements CanActivate {
     /**
      * Decorator for controller or handler to specify allowed roles.
-     * For surtain roles pass it to decorator.
+     * For certain roles pass it to decorator.
      * Pass `false` to make controller or handler be allowed only for
      * unauthorized users.
+     * Pass `true` to make controller or handler be allowed only for
+     * authorized users (with any role).
      * Do not use it to make controller or handler bew allowed for all.
      */
     static readonly Params = (...allowedRoles: RoleParams<R>) =>
@@ -51,7 +53,13 @@ export function createRolesGuard<R>(
           throw new ForbiddenException();
         }
 
-        // handler is allowed for surtain roles
+        // handler is allowed for any authorized
+      } else if (requiredRoles[0] === true) {
+        // but there is not any role
+        if (role!) {
+          throw new UnauthorizedException();
+        }
+        // handler is allowed for certain roles
       } else {
         // there is no any role
         if (!role) {
@@ -60,7 +68,7 @@ export function createRolesGuard<R>(
 
         // role is not one of requireds
         if (
-          !(requiredRoles as Exclude<RoleParams<R>, [false]>).includes(role)
+          !(requiredRoles as Exclude<RoleParams<R>, [boolean]>).includes(role)
         ) {
           throw new ForbiddenException();
         }
